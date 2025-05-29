838ee7eb712fc41f0622e4c28be77543296fbb02
sulthanmuh51 <sulthanmuh51@gmail.com>
2025-05-29 22:34:20 +0700

    added profile view and edit, uploaded package for easier deployment

--- web/app/core/Database.php
+++ web/app/core/Database.php
@@ -71,4 +71,8 @@ class Database
     {
         return $this->stmt->rowCount();
     }
+    public function lastInsertId()
+    {
+        return $this->dbh->lastInsertId();
+    }
 }
--- web/app/views/users/register.php
+++ web/app/views/users/register.php
@@ -37,50 +37,44 @@ $(document).ready(function() {
 endif ?>
 
 <!-- Main Register Interface -->
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-auto order-2">
-            <div class="container bg-primary mt-4 p-4 px-5 p-md-5 rounded-top-4">
-                <h2 class="text-light" id="registerLabel">Register New Account</h2>
+<div class="container-md d-flex justify-content-center allign-content-center h-100">
+    <div class="bg-body-secondary h-100 w-lg-50 w-75 row flex-column justify-content-center px-xl-5 px-3 py-5">
+        <div class="px-xl-5 py-5">
+            <div class="form-header mb-3 py-2 border-bottom border-dark-subtle">
+                <h3 id="registerLabel">Register New Account</h3>
             </div>
-            <div class="container bg-body-secondary p-5 rounded-bottom-4">
-                <div class="register-body">
-                    <form action="<?= BASEURL ?>/users/submitRegistration" method="POST">
-                        <input type="hidden" name="role" value="<?= $_SESSION['role'] ?? 0 ?>">
-                        <div class="mb-3">
-                            <label for="username" class="form-label">Username</label>
-                            <input required type="text" class="form-control" id="username" name="username">
-                        </div>
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input required type="email" class="form-control" id="email" name="email">
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input required type="password" class="form-control" id="password" name="password">
-                        </div>
-                        <div class="mb-3">
-                            <label for="password2" class="form-label">Confirm Password</label>
-                            <p id="password-match" class="text-danger d-none">Password didn't match</p>
-                            <input required type="password" class="form-control" id="password2">
-                        </div>
-                        <div class="d-grid mt-4">
-                            <button type="submit" class="btn btn-primary">Register</button>
-                    </form>
-                </div>
+            <div class="register-body">
+                <form action="<?= BASEURL ?>/users/submitRegistration" method="POST">
+                    <input type="hidden" name="role" value="<?= $_SESSION['role'] ?? 0 ?>">
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input required type="text" class="form-control" id="username" name="username">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input required type="email" class="form-control" id="email" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input required type="password" class="form-control" id="password" name="password">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password2" class="form-label">Confirm Password</label>
+                        <p id="password-match" class="text-danger d-none">Password didn't match</p>
+                        <input required type="password" class="form-control" id="password2">
+                    </div>
+                    <div class="d-grid mt-4">
+                        <button type="submit" class="btn btn-primary">Register</button>
+                </form>
             </div>
         </div>
     </div>
-    <div class="w-100 d-none d-md-block"></div>
-    <div class="col order-1 d-none d-md-block">
-        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda in sequi alias ea neque cumque
-            architecto doloribus quis ex cum.</h1>
-    </div>
+</div>
 </div>
 </div>
 
-<p><?php var_dump($_POST) ?></p>
 
+<!-- Ajax password-match -->
 <script>
 $(function() {
     $('#password, #password2').on('keyup', function() {

3a58765b59a3ef674834634a1320b3bde07d55dd
sulthanmuh51 <sulthanmuh51@gmail.com>
2025-06-07 08:15:40 +0700

    added some features for courses

--- web/app/controllers/Courses.php
+++ web/app/controllers/Courses.php
@@ -5,18 +5,42 @@ class Courses extends Controller
     public function index()
     {
         $data['judul'] = 'Courses';
+        $page = $_GET['page'] ?? 1;
+        $cards = $this->model('Courses_model')->loadCoursesCards($page);
         $this->view('templates/header', $data);
-        $this->view('courses/index');
+        $this->view('courses/index', $cards);
         $this->view('templates/footer');
     }
 
     public function detail()
     {
-    $data['judul'] = 'Detail Kursus';
+        $course = $this->model('Courses_model')->loadCourse($_GET['courseId']);
+        $data['judul'] = 'Course Detail';
 
-    $this->view('templates/header', $data);
-    $this->view('courses/detail', $data);
-    $this->view('templates/footer');
+        $this->view('templates/header', $data);
+        $this->view('courses/detail', $course);
+        $this->view('templates/footer');
+    }
+    public function create()
+    {
+        $data['judul'] = 'Create Course';
+
+        $this->view('templates/header', $data);
+        $this->view('courses/create');
+        $this->view('templates/footer');
     }
 
+    public function createCourse()
+    {
+        $result = $this->model('Courses_model')->createNewCourse($_POST, $_FILES['thumbnail'], $_SESSION['user_id']);
+        if ($result['success'] == true) {
+            Flasher::setFlash('Course has been successfully created.', 'success');
+            header('Location: ' . BASEURL . '/courses/detail?courseId=' . $result['courseId']);
+            exit;
+        } else {
+            Flasher::setFlash($result['error_msg'],  "danger");
+            header('Location: ' . BASEURL . '/courses/create?error=' . $result['error']);
+            exit;
+        }
+    }
 }
--- web/app/views/templates/footer.php
+++ web/app/views/templates/footer.php
@@ -5,6 +5,7 @@
 <link rel="stylesheet" href="<?= BASEURL ?>/node_modules/sweetalert2/dist/sweetalert2.min.css">
 <script src="<?= BASEURL ?>/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script defer src="<?= BASEURL ?>/js/script.js"></script>
+<script src="<?= BASEURL ?>/js/summernote-bs5.js"></script>
 </body>
 
 </html>
